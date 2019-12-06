19a0939e57fc0badf4ecb4f3dfa573ffaa3cf008
lsongzhi <lsongzhi@163.com>
2019-12-06 23:07:56 +0800

    feat: plugins,pages

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  title: '松直的博客',
+  themeConfig: {
+    sidebar: 'auto',
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '技术', link: '/tech/' },
+      { text: '随笔', link: '/essay/' }
+    ]
+  },
+  locales: {
+    '/': {
+      lang: 'zh-CN', 
+    }
+  },
+  head: [['link', { rel: 'icon', href: '/logo.png' }]],
+  plugins: [
+    '@vuepress/active-header-links',
+    '@vuepress/back-to-top',
+    '@vuepress/medium-zoom',
+    '@vuepress/nprogress'
+  ]
+}
--- docs/essay/README.md
+++ docs/essay/README.md
@@ -1,3 +1,4 @@
 ---
 sidebar: false
 ---
+# 随笔
