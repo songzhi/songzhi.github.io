9dd61f0307da083f4fbfd18cbbc20307c9e868e2
lsongzhi <lsongzhi@163.com>
2019-12-08 23:46:54 +0800

    feat: themes

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  title: '松直的博客',
+  title: "松直的博客",
   themeConfig: {
-    sidebar: 'auto',
+    sidebar: "auto",
     nav: [
-      { text: '首页', link: '/' },
-      { text: '技术', link: '/tech/' },
-      { text: '随笔', link: '/essay/' }
+      { text: "首页", link: "/" },
+      { text: "技术", link: "/tech/" },
+      { text: "想法", link: "/idea/" }
     ]
   },
   locales: {
-    '/': {
-      lang: 'zh-CN', 
+    "/": {
+      lang: "zh-CN"
     }
   },
-  head: [['link', { rel: 'icon', href: '/logo.png' }]],
+  head: [["link", { rel: "icon", href: "/logo.png" }]],
   plugins: [
-    '@vuepress/active-header-links',
-    '@vuepress/back-to-top',
-    '@vuepress/medium-zoom',
-    '@vuepress/nprogress'
+    "@vuepress/active-header-links",
+    "@vuepress/back-to-top",
+    "@vuepress/medium-zoom",
+    "@vuepress/nprogress"
   ]
-}
+};
