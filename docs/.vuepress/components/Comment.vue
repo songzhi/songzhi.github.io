60686041158418537ea2a3a611f801ffce532178
lsongzhi <lsongzhi@163.com>
2019-12-08 23:47:22 +0800

    feat:comment

---
+++ docs/.vuepress/components/Comment.vue
@@ -0,0 +1,19 @@
+<template>
+  <script
+    src="https://utteranc.es/client.js"
+    repo="songzhi/songzhi.github.io"
+    issue-term="url"
+    label="comment"
+    theme="github-light"
+    crossorigin="anonymous"
+    SameSite=None
+    async
+  >
+  </script>
+</template>
+
+<script>
+export default {
+  name: 'Comment'
+}
+</script>
