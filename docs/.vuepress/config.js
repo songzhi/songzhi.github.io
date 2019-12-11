ca31bbdd3958757867562e59af84c7b3dddfc691
lsongzhi <lsongzhi@163.com>
2019-12-11 17:42:41 +0800

    feat:markdown plugins

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -12,19 +12,41 @@ module.exports = {
       lang: "zh-CN"
     }
   },
-  head: [["link", { rel: "icon", href: "/logo.png" }]],
+  head: [
+    ["link", { rel: "icon", href: "/logo.png" }],
+    [
+      "link",
+      {
+        href: "https://cdn.jsdelivr.net/npm/katex@0.11.1/dist/katex.min.css",
+        rel: "stylesheet",
+        integrity:
+          "sha384-zB1R0rpPzHqg7Kpt0Aljp8JPLqbXI3bhnPWROx27a9N0Ll6ZP/+DiW/UqRcLbRjq",
+        crossorigin: "anonymous"
+      }
+    ]
+  ],
   plugins: [
     "@vuepress/active-header-links",
     "@vuepress/back-to-top",
     "@vuepress/medium-zoom",
     "@vuepress/nprogress",
     [
-      'vuepress-plugin-container',
+      "vuepress-plugin-container",
       {
-        type: 'details',
-        before: info => `<details class="custom-block details"><summary>${info}</summary>`,
-        after: '</details>',
-      },
-    ],
-  ]
+        type: "details",
+        before: info =>
+          `<details class="custom-block details"><summary>${info}</summary>`,
+        after: "</details>"
+      }
+    ]
+  ],
+  markdown: {
+    extendMarkdown: md => {
+      // 使用更多的 markdown-it 插件!
+      md.use(require("markdown-it-footnote"));
+      md.use(require("@liradb2000/markdown-it-katex"));
+      md.use(require("markdown-it-mark"));
+      md.use(require("markdown-it-task-lists"));
+    }
+  }
 };
